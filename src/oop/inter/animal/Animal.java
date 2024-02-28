e4b95d9f1d6d7ec9d4e32ca009c029c56ece4781
mipi1256 <mipi1258@gmail.com>
2024-02-28 16:44:29 +0900

    # Java oop (인터페이스)

---
+++ src/oop/inter/animal/Animal.java
@@ -0,0 +1,8 @@
+package oop.inter.animal;
+
+public interface Animal {
+
+   // 내가 앞으로 만들 클래스의 긴으을 명세
+   void feed(String foodName);
+   boolean sleep();
+}
---
+++ src/oop/inter/animal/Violent.java
@@ -0,0 +1,10 @@
+package oop.inter.animal;
+
+public interface Violent {
+
+   void angry();
+
+   void fight();
+
+
+}
